9da428f9b585ae806ebcb6ffd27a2ff88d2d3588
Alex Prince <zhuzepa@yandex.ru>
2026-05-07 00:14:27 +0300

    KS-3-1 string hello

---
+++ src/main/kotlin/androidsprint/string_ks-3-1.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+
+    val nameUser = "Шархан"
+    var textHello = ""
+
+    textHello = "Добрый день"
+    println("$textHello, $nameUser!")
+
+    textHello = "Добрый вечер"
+    println("$textHello, $nameUser!")
+
+
+}
