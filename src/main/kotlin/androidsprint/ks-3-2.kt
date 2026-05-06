ff218ff31c7a14cbe07e28ff1aed906b21ec24b9
Alex Prince <zhuzepa@yandex.ru>
2026-05-07 00:44:10 +0300

    KS-3-2 change last name

---
+++ src/main/kotlin/androidsprint/ks-3-2.kt
@@ -0,0 +1,13 @@
+fun main() {
+    //Андреева Татьяна Сергеевна в 20 лет
+    var lastName = "Андреева"
+    val firstName = "Татьяна"
+    val patronymic = "Сергеевна"
+    var age = 20
+
+    println("$lastName $firstName $patronymic, $age")
+    lastName = "Сидорова"
+    age = 22
+    println("$lastName $firstName $patronymic, $age")
+
+}
