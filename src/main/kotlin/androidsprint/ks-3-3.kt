f2e6094b4275a79b384f0343a65584793684d62d
Alex Prince <zhuzepa@yandex.ru>
2026-05-08 13:06:03 +0300

    изменил таблицу умножения ks-3-3

--- src/main/kotlin/androidsprint/ks-3-3.kt
+++ src/main/kotlin/androidsprint/ks-3-3.kt
@@ -1,15 +1,15 @@
 fun main() {
-    val inputNumber = readln().toInt()
+    val inputNumber = 6
     println("""
-        $inputNumber x 1 = ${inputNumber * 1}
-        $inputNumber x 2 = ${inputNumber * 2}
-        $inputNumber x 3 = ${inputNumber * 3}
-        $inputNumber x 4 = ${inputNumber * 4}
-        $inputNumber x 5 = ${inputNumber * 5}
-        $inputNumber x 6 = ${inputNumber * 6}
-        $inputNumber x 7 = ${inputNumber * 7}
-        $inputNumber x 8 = ${inputNumber * 8}
-        $inputNumber x 9 = ${inputNumber * 9}
-        $inputNumber x 10 = ${inputNumber * 10}
-    """.trimIndent())
+$inputNumber × 1 = ${inputNumber * 1}
+$inputNumber × 2 = ${inputNumber * 2}
+$inputNumber × 3 = ${inputNumber * 3}
+$inputNumber × 4 = ${inputNumber * 4}
+$inputNumber × 5 = ${inputNumber * 5}
+$inputNumber × 6 = ${inputNumber * 6}
+$inputNumber × 7 = ${inputNumber * 7}
+$inputNumber × 8 = ${inputNumber * 8}
+$inputNumber × 9 = ${inputNumber * 9}
+$inputNumber × 10 = ${inputNumber * 10}
+""".trimIndent())
 }
