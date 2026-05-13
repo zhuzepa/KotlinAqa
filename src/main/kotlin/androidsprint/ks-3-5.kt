3b6a668ca9c1245f5b1279d3f95430f9ae631a43
Alex Prince <zhuzepa@yandex.ru>
2026-05-13 23:59:06 +0300

    исправил переменные дз ks3-5

--- src/main/kotlin/androidsprint/ks-3-5.kt
+++ src/main/kotlin/androidsprint/ks-3-5.kt
@@ -2,12 +2,12 @@ fun main() {
     val inputString = "D2-D4;0"
     val allSteps = inputString.split("-", ";")
 
-    val fromSquare1 = allSteps[0]
-    val toSquare2 = allSteps[1]
-    val moveNumber3 = allSteps[2]
-    println(fromSquare1)
-    println(toSquare2)
-    println(moveNumber3)
+    val from = allSteps[0]
+    val to = allSteps[1]
+    val moveNumber = allSteps[2]
+    println(from)
+    println(to)
+    println(moveNumber)
 
 
 
