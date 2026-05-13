2e6dbe792acf0cbf7dc02cceb904f00626fb904e
Alex Prince <zhuzepa@yandex.ru>
2026-05-13 16:48:39 +0300

    обновил замечания задание ks-3-4

--- src/main/kotlin/androidsprint/ks-3-4.kt
+++ src/main/kotlin/androidsprint/ks-3-4.kt
@@ -2,11 +2,11 @@ fun main() {
     var fromSquare = "E2"
     var toSquare = "E4"
     var moveNumber = 1
-    println("[$fromSquare-$toSquare;$moveNumber]")
+    println("$fromSquare-$toSquare;$moveNumber")
 
     fromSquare = "D2"
     toSquare = "D3"
-    moveNumber ++
+    moveNumber++
     println("$fromSquare-$toSquare;$moveNumber")
 
 }
