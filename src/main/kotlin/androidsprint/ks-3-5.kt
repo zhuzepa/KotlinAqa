bc8b1cf17958ab6fdc847fb7b6a6e248b6689a7f
Alex Prince <zhuzepa@yandex.ru>
2026-05-13 23:54:42 +0300

    сделал дз ks3-5

---
+++ src/main/kotlin/androidsprint/ks-3-5.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val inputString = "D2-D4;0"
+    val allSteps = inputString.split("-", ";")
+
+    val fromSquare1 = allSteps[0]
+    val toSquare2 = allSteps[1]
+    val moveNumber3 = allSteps[2]
+    println(fromSquare1)
+    println(toSquare2)
+    println(moveNumber3)
+
+
+
+}
