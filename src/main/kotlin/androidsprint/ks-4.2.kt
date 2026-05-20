bf6bac651a1e76e7af4a67fd8cbc8c4b4d6dbbf6
Alex Prince <zhuzepa@yandex.ru>
2026-05-21 01:10:06 +0300

    KS-4-1 таможня

---
+++ src/main/kotlin/androidsprint/ks-4.2.kt
@@ -0,0 +1,19 @@
+const val CATEGORY_WEIGHT_MIN = 35
+const val CATEGORY_WEIGHT_MAX = 100
+const val VOLUME_LITRES = 100
+fun main() {
+    val weightCargo_1 = 20
+    val litres_1 = 80
+
+    val weightCargo_2 = 50
+    val litres_2 = 100
+
+
+
+    println("Груз с весом $weightCargo_1 кг и объемом $litres_1 л соответствует категории 'Average': ${weightCargo_1 > CATEGORY_WEIGHT_MIN && weightCargo_1 <= CATEGORY_WEIGHT_MAX && litres_1 < VOLUME_LITRES}")
+    println(
+        "Груз с весом $weightCargo_2 кг и объемом $litres_2 л соответствует категории 'Average': ${
+            weightCargo_2 > CATEGORY_WEIGHT_MIN && weightCargo_2 <= CATEGORY_WEIGHT_MAX && litres_2 < VOLUME_LITRES
+        }"
+    )
+}
