ebc2b70c1e49a09a63fc25aa11f8d6ba305e16ba
Alex Prince <zhuzepa@yandex.ru>
2026-05-21 01:32:24 +0300

    KS-4-2 правки таможня

--- src/main/kotlin/androidsprint/ks-4.2.kt
+++ src/main/kotlin/androidsprint/ks-4.2.kt
@@ -2,16 +2,20 @@ const val CATEGORY_WEIGHT_MIN = 35
 const val CATEGORY_WEIGHT_MAX = 100
 const val VOLUME_LITRES = 100
 fun main() {
-    val weightCargo_1 = 20
-    val litres_1 = 80
+    val weightCargo1 = 20
+    val litres1 = 80
 
-    val weightCargo_2 = 50
-    val litres_2 = 100
+    val weightCargo2 = 50
+    val litres2 = 100
 
-    println("Груз с весом $weightCargo_1 кг и объемом $litres_1 л соответствует категории 'Average': ${weightCargo_1 > CATEGORY_WEIGHT_MIN && weightCargo_1 <= CATEGORY_WEIGHT_MAX && litres_1 < VOLUME_LITRES}")
     println(
-        "Груз с весом $weightCargo_2 кг и объемом $litres_2 л соответствует категории 'Average': ${
-            weightCargo_2 > CATEGORY_WEIGHT_MIN && weightCargo_2 <= CATEGORY_WEIGHT_MAX && litres_2 < VOLUME_LITRES
+        "Груз с весом $weightCargo1 кг и объемом $litres1 л соответствует категории 'Average': ${
+            weightCargo1 > CATEGORY_WEIGHT_MIN && weightCargo1 <= CATEGORY_WEIGHT_MAX && litres1 < VOLUME_LITRES
+        }"
+    )
+    println(
+        "Груз с весом $weightCargo2 кг и объемом $litres2 л соответствует категории 'Average': ${
+            weightCargo2 > CATEGORY_WEIGHT_MIN && weightCargo2 <= CATEGORY_WEIGHT_MAX && litres2 < VOLUME_LITRES
         }"
     )
 }
