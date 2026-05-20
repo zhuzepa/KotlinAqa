41a52b93195b766218023399d968c9bcfd1760c9
Alex Prince <zhuzepa@yandex.ru>
2026-05-20 19:51:41 +0300

    KS-4-1 внес правки по бронированию

--- src/main/kotlin/androidsprint/ks-4.1.kt
+++ src/main/kotlin/androidsprint/ks-4.1.kt
@@ -1,11 +1,11 @@
-const val NUMBER_TABLES = 13
+const val NUMBER_OF_TABLES = 13
 fun main() {
 
-    val countTablesToday = 12
+    val countTablesToday = 13
     val countTablesTomorrow = 9
 
-    val isTablesToday: Boolean = NUMBER_TABLES - countTablesToday > 0
-    val isTablesTomorrow: Boolean = NUMBER_TABLES - countTablesTomorrow > 0
+    val isTablesToday = NUMBER_OF_TABLES - countTablesToday > 0
+    val isTablesTomorrow = NUMBER_OF_TABLES - countTablesTomorrow > 0
 
-    println("Доступность столиков на сегодня: $isTablesToday\nДоступность столиков на завтра: $isTablesTomorrow")
+    println("[Доступность столиков на сегодня: $isTablesToday],\n[Доступность столиков на завтра: $isTablesTomorrow]")
 }
