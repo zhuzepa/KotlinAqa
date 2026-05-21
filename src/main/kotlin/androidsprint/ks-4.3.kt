010cb5d7ade629c1923552fb88740273a1aaec47
Alex Prince <zhuzepa@yandex.ru>
2026-05-21 23:21:20 +0300

    ks-4-3 внес правки урожай

--- src/main/kotlin/androidsprint/ks-4.3.kt
+++ src/main/kotlin/androidsprint/ks-4.3.kt
@@ -1,12 +1,12 @@
 const val FAVORABLE_HUMIDITY = 20
 
 fun main() {
-    val weatherToday = true
-    val tentOpen = true
+    val isWeatherToday = true
+    val isTentOpen = true
     val humidity = 20
     val season = "зима"
 
-    val result = weatherToday && tentOpen && humidity == FAVORABLE_HUMIDITY && season != "зима"
+    val result = isWeatherToday && isTentOpen && humidity == FAVORABLE_HUMIDITY && season != "зима"
 
     println("Благоприятные ли условия сейчас для роста бобовых? $result")
 }
