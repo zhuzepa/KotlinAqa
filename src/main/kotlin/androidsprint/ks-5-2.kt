349fa927a24ff50a851b0454d270f92d5fe5321c
Alex Prince <zhuzepa@yandex.ru>
2026-05-26 00:50:56 +0300

    ks-5-2 правки возраста

--- src/main/kotlin/androidsprint/ks-5-2.kt
+++ src/main/kotlin/androidsprint/ks-5-2.kt
@@ -1,10 +1,10 @@
     const val AGE_OF_MAJORITY = 18
-    const val YEAR = 2026
     fun main() {
+        val year = 2026
         println("Введите год рождения: ")
-        val inputAge = readln().toInt()
+        val yearBorn = readln().toInt()
 
-        val ageUser = YEAR - inputAge
+        val ageUser = year - yearBorn
 
         if (ageUser >= AGE_OF_MAJORITY) {
             println("Показать экран со скрытым контентом.")
