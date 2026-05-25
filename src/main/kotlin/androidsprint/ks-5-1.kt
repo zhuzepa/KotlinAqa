99e3507c24866b33748989532a8839ad3e0fc276
Alex Prince <zhuzepa@yandex.ru>
2026-05-26 00:04:07 +0300

    ks-5-1 правки по сложкеию

--- src/main/kotlin/androidsprint/ks-5-1.kt
+++ src/main/kotlin/androidsprint/ks-5-1.kt
@@ -2,12 +2,11 @@ fun main() {
     val num1 = (1..10).random()
     val num2 = (1..10).random()
 
-    println("Докажите что вы не бот")
-    println("Сколько будет $num1 + $num2")
+    println("Подтвердите, что вы не робот, сложите два числа: $num1 и $num2")
 
-    val userAnswet = readln().toIntOrNull()
+    val userAnswer = readln().toIntOrNull()
 
-    if (userAnswet == num1 + num2) {
+    if (userAnswer == num1 + num2) {
         println("Добро пожаловать!")
     } else println("Доступ запрещен.")
 
