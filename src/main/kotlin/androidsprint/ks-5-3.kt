ddee20cb4164a426644ba525c047bb8e4616517b
Alex Prince <zhuzepa@yandex.ru>
2026-05-27 00:12:49 +0300

    ks-5-3 лотерея правки

--- src/main/kotlin/androidsprint/ks-5-3.kt
+++ src/main/kotlin/androidsprint/ks-5-3.kt
@@ -1,21 +1,18 @@
 const val ONE_NUMBER = 18
-const val TWO_NUMBER = 63
+const val TWO_NUMBER = 42
 
 fun main() {
     println("Введите первое число от 0 до 42: ")
     val inputOneNumber = readln().toInt()
     println("Введите второе число от 0 до 42: ")
     val inputTwoNumber = readln().toInt()
+    println("Правильные числа: $ONE_NUMBER и $TWO_NUMBER")
 
-     println("Правильные числа: $ONE_NUMBER и $TWO_NUMBER")
+    val firstIsCorrect = inputOneNumber == ONE_NUMBER || inputOneNumber == TWO_NUMBER
+    val secondIsCorrect = inputTwoNumber == ONE_NUMBER || inputTwoNumber == TWO_NUMBER
 
-    val bothMatch = (inputOneNumber == ONE_NUMBER && inputTwoNumber == TWO_NUMBER) ||
-            (inputOneNumber == TWO_NUMBER && inputTwoNumber == ONE_NUMBER)
-
-    val oneMatch = (inputOneNumber == ONE_NUMBER || inputOneNumber == TWO_NUMBER) &&
-            (inputTwoNumber != ONE_NUMBER && inputTwoNumber != TWO_NUMBER) ||
-            (inputTwoNumber == ONE_NUMBER || inputTwoNumber == TWO_NUMBER) &&
-            (inputOneNumber != ONE_NUMBER && inputOneNumber != TWO_NUMBER)
+    val bothMatch = firstIsCorrect && secondIsCorrect
+    val oneMatch = (firstIsCorrect && !secondIsCorrect) || (!firstIsCorrect && secondIsCorrect)
 
     if (bothMatch) {
         println("Поздравляем! Вы выиграли главный приз!")
