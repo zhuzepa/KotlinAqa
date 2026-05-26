55ae09fe53b3a7ce085c192016c483f407af723f
Alex Prince <zhuzepa@yandex.ru>
2026-05-27 00:20:29 +0300

    ks-5-3 лотерея правки

--- src/main/kotlin/androidsprint/ks-5-3.kt
+++ src/main/kotlin/androidsprint/ks-5-3.kt
@@ -1,15 +1,15 @@
-const val ONE_NUMBER = 18
-const val TWO_NUMBER = 42
+const val FIRST_WINNING_NUMBER = 18
+const val TWO_WINNING_NUMBER = 42
 
 fun main() {
     println("Введите первое число от 0 до 42: ")
     val inputOneNumber = readln().toInt()
     println("Введите второе число от 0 до 42: ")
     val inputTwoNumber = readln().toInt()
-    println("Правильные числа: $ONE_NUMBER и $TWO_NUMBER")
+    println("Правильные числа: $FIRST_WINNING_NUMBER и $TWO_WINNING_NUMBER")
 
-    val firstIsCorrect = inputOneNumber == ONE_NUMBER || inputOneNumber == TWO_NUMBER
-    val secondIsCorrect = inputTwoNumber == ONE_NUMBER || inputTwoNumber == TWO_NUMBER
+    val firstIsCorrect = inputOneNumber == FIRST_WINNING_NUMBER || inputOneNumber == TWO_WINNING_NUMBER
+    val secondIsCorrect = inputTwoNumber == FIRST_WINNING_NUMBER || inputTwoNumber == TWO_WINNING_NUMBER
 
     val bothMatch = firstIsCorrect && secondIsCorrect
     val oneMatch = (firstIsCorrect && !secondIsCorrect) || (!firstIsCorrect && secondIsCorrect)
