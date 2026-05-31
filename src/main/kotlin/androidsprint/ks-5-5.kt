b1f78584e5ee036aedcc3f0b18398d9f7ca8f1b5
Alex Prince <zhuzepa@yandex.ru>
2026-05-31 16:35:28 +0300

    ks-5-5 лотерея

---
+++ src/main/kotlin/androidsprint/ks-5-5.kt
@@ -0,0 +1,26 @@
+import kotlin.random.Random
+
+fun main() {
+    val userNumbers = mutableListOf<Int>()
+    println("Введите первое число: ")
+    userNumbers.add(readln().toInt())
+    println("Введите второе число: ")
+    userNumbers.add(readln().toInt())
+    println("Введите третье число: ")
+    userNumbers.add(readln().toInt())
+
+    val winningNumbers = List(3) {
+        Random.nextInt(0, 43)
+    }
+
+    val matchCount = winningNumbers.intersect(userNumbers).size
+
+    when (matchCount) {
+        3 -> println("угадал все числа и выиграл джекпот.")
+        2 -> println("угадал два числа и получает крупный приз.")
+        1 -> println("пользователю выплачивается утешительный приз.")
+        else -> println("пользователь не угадал ни одного числа.")
+    }
+    println("Выигрышные числа: $winningNumbers")
+
+}
