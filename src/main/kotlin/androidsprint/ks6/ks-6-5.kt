815d0f81fa3b976c6228233c7d9fcdf8bc673065
Alex Prince <zhuzepa@yandex.ru>
2026-06-13 16:41:57 +0300

    ks-6-4 капча правки

--- src/main/kotlin/androidsprint/ks6/ks-6-5.kt
+++ src/main/kotlin/androidsprint/ks6/ks-6-5.kt
@@ -1,21 +1,20 @@
-import kotlin.random.Random
-
 fun main() {
     var attempts = 0
+    var success = false
     while (attempts < 3) {
-        val randomNumberOne = Random.nextInt(1, 10)
-        val randomNumberTwo = Random.nextInt(1, 10)
+        val randomNumberOne = (1..9).random()
+        val randomNumberTwo = (1..9).random()
         println("Сколько будет $randomNumberOne + $randomNumberTwo ?")
         val responseUser = readln().toInt()
         if (responseUser == (randomNumberOne + randomNumberTwo)) {
             println("Добро пожаловать!")
+            success = true
             break
         }
         attempts++
-        if (attempts < 3) {
-            println("Попробуйте еще раз")
-            continue
-        }
+        println("Попробуйте еще раз")
     }
-    println("Доступ запрещен")
+    if (!success) {
+        println("Доступ запрещен")
+    }
 }
