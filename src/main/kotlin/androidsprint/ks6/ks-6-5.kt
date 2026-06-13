85f9a013d853b5e2bbe6e20da7f69c1b206c701c
Alex Prince <zhuzepa@yandex.ru>
2026-06-13 16:58:12 +0300

    ks-6-4 капча правки

--- src/main/kotlin/androidsprint/ks6/ks-6-5.kt
+++ src/main/kotlin/androidsprint/ks6/ks-6-5.kt
@@ -1,12 +1,13 @@
 fun main() {
     var attempts = 0
     var success = false
+
     while (attempts < 3) {
         val randomNumberOne = (1..9).random()
         val randomNumberTwo = (1..9).random()
         println("Сколько будет $randomNumberOne + $randomNumberTwo ?")
         val responseUser = readln().toInt()
-        if (responseUser == (randomNumberOne + randomNumberTwo)) {
+        if (responseUser == randomNumberOne + randomNumberTwo) {
             println("Добро пожаловать!")
             success = true
             break
